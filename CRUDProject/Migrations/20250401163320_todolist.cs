b0b652bf8fb732b621c7e38b729ca85e4e026b5c
Nuttaphon <nathn2420@gmail.com>
2025-04-01 23:39:34 +0700

    setup project and first update

---
+++ CRUDProject/Migrations/20250401163320_todolist.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUDProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class todolist : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoLists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Task = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoLists", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoLists");
+        }
+    }
+}
---
+++ CRUDProject/Models/ToDoList.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace CRUDProject.Models
+{
+	public class ToDoList
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public string Task { get; set; }
+
+		public string Description { get; set; }
+
+		[Required]
+		public string Status { get; set; } = "Pending";
+
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+		public DateTime UpdatedAt { get; set; }
+	}
+}
